91408ee7e3f2edab7037992cd39446ba22c1dc2b
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-03-07 23:32:50 -0600

    bash -> start

---
+++ helloworld.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+: 'este es un comentario de
+varias líneas, que prueba que no sólo
+sirven los hastags para poder comentar algo
+que deseemos, sino también este tipo de comentarios
+funciona para hacerlo'
+
+cat >> hola.txt
