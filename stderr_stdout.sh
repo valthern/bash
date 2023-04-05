7d32e8f7ecd83151e0c64dc6d6383d1e30957d3c
Omar I. Martínez Gómez <omar_238@hotmail.com>
2023-04-04 23:12:53 -0600

    youtube -> adición de comentario en el archivo stderr_stdout.sh

--- stderr_stdout.sh
+++ stderr_stdout.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
+# Se guarda la salida estandard en el primer parámetro y los errores en el segundo
 #ls -las 1>file.txt 2>error.txt
 
 # Se guarda la salida estandard en el primer parámetro y el error en el primer parámetro también
